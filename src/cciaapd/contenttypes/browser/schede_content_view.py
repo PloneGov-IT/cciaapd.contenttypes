14e99c92948a7d9473677ccf730545fc21e55cc4
daniele-andreotti <daniele.andreotti@redturtle.it>
2015-08-12 17:07:03 +0200

    Improved porltet behavior when showing contents

--- src/cciaapd/contenttypes/browser/schede_content_view.py
+++ src/cciaapd/contenttypes/browser/schede_content_view.py
@@ -1,5 +1,6 @@
 from Products.Five import BrowserView
 from zope.component import getMultiAdapter
+from Products.CMFCore.interfaces import ISiteRoot
 
 
 class SchedeContentView(BrowserView):
@@ -9,12 +10,20 @@ class SchedeContentView(BrowserView):
         """
         return self.request.get('portal_type')
 
+    def get_view(self, parent, view_name):
+        view = getMultiAdapter(
+            (parent, self.request),
+            name=view_name
+        )
+        return view
+
     def get_default_view_object(self):
         ''' Check for default page and get it from the context
         if default page is not set return None, otherwise the object
         '''
         default_page = self.context.getDefaultPage()
-        return self.context.get(default_page)
+        obj = self.context.get(default_page)
+        return obj
 
     def is_default_view(self):
         """Return true if the default view is set to Scheda"""
@@ -28,8 +37,11 @@ class SchedeContentView(BrowserView):
         return self.context.portal_type == "Scheda"
 
     def find_nephews(self, obj=None):
-        ''' Check inside objects all the filtered types and
-        return their children
+        '''
+        Check inside objects all the filtered types and
+        return their children.
+        Moreover, parent folders must be traversed to get contents of the
+        first one where the default view is set as Scheda, if exists.
         '''
         if obj is None:
             obj = self.context
@@ -40,36 +52,52 @@ class SchedeContentView(BrowserView):
         [results.extend(child.listFolderContents()) for child in children]
 
         parent = self.context.aq_parent
-        view = getMultiAdapter(
-            (parent, self.request),
-            name=u'schede_content_view'
-        )
-        results.extend(view.get_results())
+        view = self.get_view(parent, u'schede_content_view')
 
-        return set(results).sort()
+        results.extend(view.get_results(original=False))
 
-    def get_results(self):
+        return sorted(set(results))
+
+    def get_next(self):
+        """Find next interesting object in the aq_chain"""
+        for obj in self.context.aq_chain:
+            view = self.get_view(obj, u'schede_content_view')
+
+            if ISiteRoot.providedBy(obj):
+                return
+            if view.is_scheda() or view.is_default_view():
+                return obj
+
+    def get_results(self, original=True):
         """Returns results depending on request and the following logic
 
         Case 1: a folder with a Scheda set as  default view ->
-                                scheda contents filtered by content_types
+                return scheda contents filtered by content_types
 
         Case 2: the context is a Scheda ->
                 return the contents of Moduli or Riferimenti according
                 to the requeste portal type
 
-        Fallback: as a fallback return an empty list
+        Case 3: as a fallback return an empty list
         """
 
-        import pdb;pdb.set_trace()
+        if ISiteRoot.providedBy(self.context):
+            return []
 
         if self.is_default_view():
             return self.find_nephews(self.get_default_view_object())
 
         if self.is_scheda():
             return self.find_nephews()
+        if original:
+            return []
+        parent = self.get_next()
+        if parent is None:
+            return []
 
-        return []
+        view = self.get_view(parent, u'schede_content_view')
+
+        return view.get_results()
 
     def __call__(self):
         """"""
