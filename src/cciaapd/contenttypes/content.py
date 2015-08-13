c18b3e98ee5644910a3a04b3d7519c1bc313bd7c
daniele-andreotti <daniele.andreotti@redturtle.it>
2015-08-13 14:19:18 +0200

    Added translations

--- src/cciaapd/contenttypes/content.py
+++ src/cciaapd/contenttypes/content.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from zope.interface import implementer
 from interfaces import IScheda
+from interfaces import IUfficio
 from plone.dexterity.content import Container
+from plone.dexterity.content import Item
 
 
 @implementer(IScheda)
@@ -9,3 +11,9 @@ class Scheda(Container):
 
     """Convenience subclass for ``Collection`` portal type
     """
+
+
+@implementer(IUfficio)
+class Ufficio(Item):
+
+    """Class for Ufficio """
--- src/cciaapd/contenttypes/interfaces.py
+++ src/cciaapd/contenttypes/interfaces.py
@@ -3,6 +3,8 @@
 
 from cciaapd.contenttypes import _
 from plone.app.textfield import RichText
+from zope.schema import TextLine
+
 from plone.supermodel import model
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
@@ -37,3 +39,56 @@ class IModuliFolderSchema(model.Schema):
 class IRiferimentiFolderSchema(model.Schema):
 
     """ Schema for RiferimentiFolder """
+
+
+class IUfficio(Interface):
+
+    """ Marker interface """
+
+
+class IUfficioSchema(model.Schema):
+
+    location = TextLine(
+        title=_(u"Location"),
+        default=u"",
+        required=False
+    )
+
+    phone = TextLine(
+        title=_(u"Phone"),
+        default=u"",
+        required=False
+    )
+
+    fax = TextLine(
+        title=_(u"Fax"),
+        default=u"",
+        required=False
+    )
+
+    email = TextLine(
+        title=_(u"Email"),
+        default=u"",
+        required=False
+    )
+
+    pec = TextLine(
+        title=_(u"Pec"),
+        default=u"",
+        required=False
+    )
+
+    executive = TextLine(
+        title=_(u"Executive"),
+        default=u"",
+        required=False
+    )
+
+    competenze = RichText(
+        title=_(u"Duties"),
+        default_mime_type='text/html',
+        output_mime_type='text/html',
+        allowed_mime_types=('text/html', 'text/plain',),
+        default=u"",
+        required=False
+    )
