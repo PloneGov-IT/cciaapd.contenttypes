3e944761a983c5782114a8a20fc8ac42ff9a3df0
daniele-andreotti <daniele.andreotti@redturtle.it>
2015-08-13 15:54:45 +0200

    Filtered Scheda related office field in order to select just Ufficio types

--- src/cciaapd/contenttypes/behaviors/related_office.py
+++ src/cciaapd/contenttypes/behaviors/related_office.py
@@ -8,6 +8,7 @@ from plone.supermodel import model
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope.interface import alsoProvides
+from ..interfaces import IUfficio
 
 
 class IRelatedOffice(model.Schema):
@@ -15,12 +16,14 @@ class IRelatedOffice(model.Schema):
     ''' Behavior interface
     '''
     form.widget(related_office=MultiContentTreeFieldWidget)
+
     related_office = RelationList(
         title=_(u'label_related_office', default=u'Related office'),
         default=[],
         value_type=RelationChoice(
             title=u"Related",
-            source=ObjPathSourceBinder(),
+            source=ObjPathSourceBinder(
+                object_provides=IUfficio.__identifier__),
         ),
         required=False
     )
