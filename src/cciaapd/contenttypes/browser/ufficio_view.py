a6ba4201046a2a2e21e6dc4458b9049c3c7f2ce6
daniele-andreotti <daniele.andreotti@redturtle.it>
2015-10-23 16:14:00 +0200

    Refactored the mrthod in order to get from the current office back references form both kind of types (AT and DX)

--- src/cciaapd/contenttypes/browser/ufficio_view.py
+++ src/cciaapd/contenttypes/browser/ufficio_view.py
@@ -6,25 +6,49 @@ from zope.component import getUtility
 from zope.intid.interfaces import IIntIds
 from zope.security import checkPermission
 from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.interfaces import referenceable
 
 
 class UfficioView(BrowserView):
 
     @memoize
     def back_references(self):
-        ''' Where this expert was related
+        ''' Return back references both of Dexterity and AT items
         '''
+
+        result = []
+        result.extend(self.get_back_references_from_AT_type())
+        result.extend(self.get_back_references_from_DX_type())
+
+        return sorted(result, key=lambda x: x.Title())
+
+
+    def get_back_references_from_AT_type(self):
+        """ Returns back references for Archetypes items """
+
+        result = []
+        referenceable_item = referenceable.IReferenceable(self.context)
+        result.extend(referenceable_item.getBRefs())
+
+        return result
+
+    def get_back_references_from_DX_type(self):
+        """ Returns back references for Dexterity items """
+
         catalog = getUtility(ICatalog)
         intids = getUtility(IIntIds)
-        result = []
+
         query = {
             'to_id': intids.getId(aq_inner(self.context)),
             'from_attribute': 'related_office',
         }
+
+        result = []
         for rel in catalog.findRelations(query):
             obj = intids.queryObject(rel.from_id)
             if obj is not None and checkPermission('zope2.View', obj):
                 result.append(obj)
+                
         return result
 
     def get_field_value(self, field_name):
