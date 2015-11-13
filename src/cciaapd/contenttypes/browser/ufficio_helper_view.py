d4b445304f284789855ac4d5006de12075f949cb
Andrea Cecchi <andrea.cecchi85@gmail.com>
2015-11-13 15:34:27 +0100

    fix view

--- src/cciaapd/contenttypes/browser/ufficio_helper_view.py
+++ src/cciaapd/contenttypes/browser/ufficio_helper_view.py
@@ -1,16 +1,21 @@
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
 from plone.dexterity.interfaces import IDexterityContent
+
 
 class UfficioHelperView(BrowserView):
 
     def get_results(self):
-        """ Returns related offices depending on the type of current item (AT or DX) """
+        """
+        Returns related offices depending on the type of current item (AT or DX)
+        """
 
         if IDexterityContent.providedBy(self.context):
             related_office_list = [x.to_object for x in self.context.related_office]
         else:
-            related_office_list = self.context.getField("related_office").get(self.context)
+            related_office_field = self.context.getField("related_office")
+            if not related_office_field:
+                return []
+            related_office_list = related_office_field.get(self.context)
 
         if len(related_office_list) == 0:
             return[]
